b410f44b51b0ef154ad1ac0aa54d556b4233fcec
wjsekdud14728 <56917697+wjsekdud14728@users.noreply.github.com>
2021-01-14 17:41:54 +0900

    php 적용하여 head, foot 분리

---
+++ foot.php
@@ -0,0 +1,21 @@
+</main>
+
+<!-- 바텀 바 시작 -->
+
+<footer class="bottom-bar con-min-width">
+    <div class="con flex">
+        <a href="#" class="logo">
+            <span><i class="fas fa-child"></i></span>
+            <span>DESIGNER DA0</span>
+        </a>
+        <div class="bottom-bar__info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit ullam? Consectetur pariatur rerum, libero nesciunt totam obcaecati fugiat repellat inventore, sed incidunt minima optio quidem quasi dolore vitae laborum!</div>
+    </div>
+</footer>
+</div>
+
+<!-- 제이쿼리 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="common.js"></script>
+<script src="index.js"></script>
+</body>
+</html>
--- head.php
+++ head.php
@@ -53,13 +53,13 @@
     <nav class="top-bar__menu-box-1 height-100p">
       <ul class="flex height-100p">
         <li>
-          <a href="#" class="flex flex-jc-c flex-ai-c height-100p">
+          <a href="/" class="flex flex-jc-c flex-ai-c height-100p">
             <span><i class="fas fa-home"></i></span>
             <span>HOME</span>
           </a>
         </li>
         <li>
-          <a href="about.html" class="flex flex-jc-c flex-ai-c height-100p">
+          <a href="about.php" class="flex flex-jc-c flex-ai-c height-100p">
             <span><i class="far fa-address-card"></i></span>
             <span>ABOUT</span>
           </a>
@@ -136,87 +136,3 @@
 </aside>
 <!-- 모바일 사이드 바 끝 -->
 <main>
-
-  <section class="section-title con-min-width">
-    <h1 class="con">
-      <span>
-        <i class="fas fa-fire"></i>
-      </span>
-      <span>
-        LATEST ARTICLES
-      </span>
-    </h1>
-  </section>
-
-  <section class="section-latest-articles con-min-width">
-    <div class="con">
-      <div class="article-list-box">
-        <ul>
-          <li>
-            <h1 class="article-list-box__title">자바스크립트 사용법</h1>
-            <div class="article-list-box__reg-date">2020-01-12 12:12:14</div>
-            <div class="article-list-box__body">
-              <script type="text/x-template">
-                # 자바스크립트 사용법
-```html
-<<!--REPLACE:script-->>
-var a = 10;
-</<!--REPLACE:script-->>
-<div class="a"></div>
-```
-              </script>
-              <div class="toast-ui-viewer"></div>
-            </div>
-          </li>
-          <li>
-            <h1 class="article-list-box__title">코드팬 사용법</h1>
-            <div class="article-list-box__reg-date">2020-01-12 12:12:14</div>
-            <div class="article-list-box__body">
-              <script type="text/x-template">
-                # 코드팬 사용법
-```codepen
-https://codepen.io/wjsekdud789/embed/Exgdagw?height=265&theme-id=light&default-tab=html,result
-```
-              </script>
-              <div class="toast-ui-viewer"></div>
-            </div>
-          </li>
-          <li>
-            <h1 class="article-list-box__title">유튜브 사용법</h1>
-            <div class="article-list-box__reg-date">2020-01-12 12:12:14</div>
-            <div class="article-list-box__body">
-              <script type="text/x-template">
-                # 유튜브 사용법
-```youtube
-r8czeYIyJ1k
-```
-              </script>
-              <div class="toast-ui-viewer"></div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
-
-</main>
-
-<!-- 바텀 바 시작 -->
-
-<footer class="bottom-bar con-min-width">
-    <div class="con flex">
-        <a href="#" class="logo">
-            <span><i class="fas fa-child"></i></span>
-            <span>DESIGNER DA0</span>
-        </a>
-        <div class="bottom-bar__info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit ullam? Consectetur pariatur rerum, libero nesciunt totam obcaecati fugiat repellat inventore, sed incidunt minima optio quidem quasi dolore vitae laborum!</div>
-    </div>
-</footer>
-</div>
-
-<!-- 제이쿼리 불러오기 -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="common.js"></script>
-<script src="index.js"></script>
-</body>
-</html>
