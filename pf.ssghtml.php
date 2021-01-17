2e8858ef402ebd5f97ad1b1f97827c5f4deb4c16
wjsekdud14728 <56917697+wjsekdud14728@users.noreply.github.com>
2021-01-18 00:11:23 +0900

    포트폴리오 페이지 생성

--- pf.ssghtml.php
+++ pf.ssghtml.php
@@ -1,9 +1,9 @@
 <?php
-require_once "heade.php";
+require_once "head.php";
 ?>
 
-<link rel="stylesheet" href="css/about.css">
 <script src="js/about.js" defer></script>
+<link rel="stylesheet" href="css/about.css">
 
 <section class="section-title con-min-width">
   <h1 class="con">
@@ -11,10 +11,35 @@ require_once "heade.php";
       <i class="fas fa-list-ol"></i>
     </span>
     <span>
-      PORFOLIO
+      PORTFOLIO
     </span>
   </h1>
 </section>
+
+</main>
+
+<!-- 바텀 바 시작 -->
+
+<footer class="bottom-bar con-min-width">
+  <div class="con flex">
+    <a href="#" class="logo">
+      <span><i class="fas fa-child"></i></span>
+      <span>DESIGNER DA0</span>
+    </a>
+    <div class="bottom-bar__info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, impedit ullam?
+      Consectetur pariatur rerum, libero nesciunt totam obcaecati fugiat repellat inventore, sed incidunt minima optio
+      quidem quasi dolore vitae laborum!</div>
+  </div>
+</footer>
+</div>
+
+<!-- 제이쿼리 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="common.js"></script>
+<script src="about.js"></script>
+</body>
+
+</html>
 
 <?php
 require_once "foot.php";
