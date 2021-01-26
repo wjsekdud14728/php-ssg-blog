f6542b43b43057bf2acd2292b8cbceb3af7f0aee
wjsekdud14728 <56917697+wjsekdud14728@users.noreply.github.com>
2021-01-26 18:10:50 +0900

    util.php, app.php 생성 data.php 수정

--- data.php
+++ data.php
@@ -1,4 +1,6 @@
 <?php
+require_once "util.php";
+require_once "app.php";
 
 $siteTitle = "디자이너 전다영";
 
@@ -9,7 +11,7 @@ $article2["title"] = "2화, 숫자와 문장";
 $article2["regDate"] = "2021-01-12 18:33:42";
 $article2["writerName"] = "전다영";
 $article2["writerAvatar"] = '<svg viewBox="0 0 264 280"><use xlink:href="#avatar-1"></use></svg>';
-$article2["body"] = <<<EOT
+$article2["body"] = <<<'EOT'
 # 숫자와 문장의 연산
 ```javascript
 //숫자 + 숫자
@@ -41,7 +43,7 @@ $article1["title"] = "1화, 출력";
 $article1["regDate"] = "2021-01-12 18:33:17";
 $article1["writerName"] = "전다영";
 $article1["writerAvatar"] = '<svg viewBox="0 0 264 280"><use xlink:href="#avatar-1"></use></svg>';
-$article1["body"] = <<<EOT
+$article1["body"] = <<<'EOT'
 
 ```html
 <t-script>
@@ -56,10 +58,35 @@ https://codepen.io/wjsekdud789/embed/ExgdVja?height=265&theme-id=dark&default-ta
 ```
 EOT;
 
-if ( isset($articleId) ){
-    $articleVarName = "article" . $articleId;
-    $selectedArticle = $$articleVarName;
+// 데이터 정리
+$maxArticleId = getMaxArticleId();
+
+$_allArticles = [];
+$_tags = [];
+
+for ( $i = $maxArticleId; $i > 0; $i-- ) {
+    $varName = 'article' . $i;
+
+    if ( isset($$varName) ) {
+        $_allArticles[${$varName}['id']] = &$$varName;
+
+        foreach ( $_allArticles[${$varName}['id']]['tags'] as $tag ) {
+            $_tags[] = $tag;
+        }
+    }
 }
 
+$_tags = array_unique($_tags);
+sort($_tags);
 
+$_allArticlesByTag = [];
 
+foreach ( $_tags as $tag ) {
+    $_allArticlesByTag[$tag] = [];
+
+    foreach ( $_allArticles as $article ) {
+        if ( in_array($tag, $article['tags']) ) {
+            $_allArticlesByTag[$tag][$article['id']] = $article;
+        }
+    }
+}
