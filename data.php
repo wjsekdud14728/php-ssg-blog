762d030b723d77b4f33e221d658c5fb42ad03e8c
wjsekdud14728 <56917697+wjsekdud14728@users.noreply.github.com>
2021-01-28 15:44:30 +0900

    링크이미지 설정

--- data.php
+++ data.php
@@ -3,6 +3,19 @@ require_once "util.php";
 require_once "app.php";
 
 $siteTitle = "디자이너 전다영";
+$siteDescription = "디자이너 전다영의 기술 블로그 입니다.";
+$siteKeywordsStr = "CSS, HTML, JS";
+$siteName = "DESIGN LEMON";
+$siteThumbUrl = "https://ssg-2020-12.oa.gg/img/logo/logo_opengraph.png";
+
+// 태그정보 시작
+$tagInfos = [
+    "flex" => [
+        "pageThumbUrl" => "자바스크립트 관련 글 전용 이미지",
+        //"pageDescription" => "2021년 특별기획, 자바스크립트 관련 강좌 입니다."
+    ],
+];
+// 태그정보 끝
 
 // 게시물2
 $article2 = [];
