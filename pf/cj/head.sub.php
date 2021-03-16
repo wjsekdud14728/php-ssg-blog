3d54b163eabf60ba1b5f987fed0bbc516d8d35f0
wjsekdud14728 <wjsekdud14728@gmail.com>
2021-03-16 16:38:50 +0900

    head 엘리먼트 중복요소 제거

--- pf/cj/head.sub.php
+++ pf/cj/head.sub.php
@@ -2,22 +2,7 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CJ 올리브네트웍스</title>
-
-  <!-- 스와이프 -->
-  <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-  <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
-  <!-- 제이쿼리 -->
-  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script defer src="common.js"></script>
-
-  <link rel="stylesheet" href="common.css">
-
-
+<?php require_once "inHead.php"; ?>
 </head>
 
 <body>
