6e6d66e7fbd5db5b204bfbdfa3ea6f444f6af0b3
unknown <56917697+wjsekdud14728@users.noreply.github.com>
2021-03-17 14:51:28 +0900

    .

--- pf.ssghtml.php
+++ pf.ssghtml.php
@@ -34,7 +34,7 @@ require_once "head.php";
         </li>
 
         <li>
-        <a href="pf/cj/index.html">
+        <a href="pf/cj/index.index.html">
         cj 올리브네트웍스 리디자인 페이지
         </a>
         </li>
