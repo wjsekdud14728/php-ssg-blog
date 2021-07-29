fbf8c94eee1ea9d8915712a504489701af6c0f71
wjsekdud14728 <56917697+wjsekdud14728@users.noreply.github.com>
2021-07-29 15:39:52 +0900

    section-3 반응형 잠시중단(넘 어려움 ㅠ), section-4 반응형 완성

--- docs/pf/cj/common.js
+++ docs/pf/cj/common.js
@@ -513,6 +513,7 @@ Section2SliderBox4__init();
 
     // 섹션3 메인아티클 영역안에서 스크롤 따라오기
 
+    
     const containerBoxBorderTopWidth__2 = parseInt($('.section-3__articles-box').css('border-top-width'));
     const containerBoxBorderBottomWidth__2 = parseInt($('.section-3__articles-box').css('border-bottom-width'));
     
@@ -522,6 +523,23 @@ Section2SliderBox4__init();
       start: containerBoxBorderTopWidth__2 + "px 0",
       end: () => "bottom " + ($('.section-3__main-article-box').outerHeight() + containerBoxBorderBottomWidth__2) + "px",
     })
+
+
+    // "(min-width: 770px)": function() {
+
+    //   let tl = ScrollTrigger.create({
+    //     trigger:'.section-3__articles-box',
+    //     pin:'.section-3__main-article-box',
+    //     start: containerBoxBorderTopWidth__2 + "px 0",
+    //     end: () => "bottom " + ($('.section-3__main-article-box').outerHeight() + containerBoxBorderBottomWidth__2) + "px",
+    //   })
+    // }, 
+    
+
+    // $(window).resize(function(){
+    //   if($(window).width()<770){
+    //   }
+    // });
 
 
 
